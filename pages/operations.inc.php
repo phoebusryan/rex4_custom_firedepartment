02faf107a9cbaba9555c652256b702bbfc4886fa
Thomas Kaegi <tk@meeloo.ch>
2016-03-21 15:53:42 +0100

    Übersetzungen für Seite 'operations' eingefügt

--- pages/operations.inc.php
+++ pages/operations.inc.php
@@ -1,13 +1,9 @@
 <?php
-	//todo:
-	//- no rows message hinzufügen
-	//- diverse übersetzungen
-	
 	$id = rex_request('id', 'int');
 	
 	switch ($func) {
 		case '':
-			$list = rex_list::factory('SELECT `id`, `start_date` FROM `'.$REX['TABLE_PREFIX'].'firedepartment_operations` ORDER BY `end_date` DESC');
+			$list = rex_list::factory('SELECT `id`, `report_short`, `start_date` FROM `'.$REX['TABLE_PREFIX'].'firedepartment_operations` ORDER BY `end_date` DESC');
 			
 			$imgHeader = '<a href="'. $list->getUrl(['func' => 'add']) .'"><img src="media/metainfo_plus.gif" alt=""/></a>';
 			
@@ -15,7 +11,10 @@
 				$imgHeader, '<img src="media/metainfo.gif" alt="" />', 0, [ '<th class="rex-icon">###VALUE###</th>', '<td class="rex-icon">###VALUE###</td>' ]
 			);	
 			
-			$list->setColumnLabel('start_date', 'Einsatzbeginn'); //todo: Translate
+			$list->setColumnLabel('report_short', 'Kurzbericht'); //todo: Translate
+			$list->setColumnParams('report_short', ['func' => 'edit', 'id' => '###id###']);
+			
+			$list->setColumnLabel('start_date', 'Alarmierung'); //todo: Translate
 			$list->setColumnParams('start_date', ['func' => 'edit', 'id' => '###id###']);
 			
 			$list->removeColumn('id');
@@ -34,7 +33,7 @@
 			
 			//Start - field for alert_id
 				$field = &$form->addSelectField('config_alert_id');
-				$field->setLabel('Alert ID'); //todo: translate
+				$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_config_alert_id_label'));
 				$select = &$field->getSelect();
 				$select->setSize(1);
 				
@@ -42,20 +41,20 @@
 				$select->addSqlOptions($query);
 			//End - field for alert_id
 			
-			//Start - field for unit_id
+			//Start - field for unit_ids
 				$field = &$form->addSelectField('config_unit_ids');
-				$field->setLabel('Unit ID'); //todo: translate
+				$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_config_unit_ids_label'));
 				$field->setAttribute('multiple', 'multiple');
 				$select = &$field->getSelect();
 				$select->setSize(5);
 				
 				$query = 'SELECT `name` as label, `id` FROM `'.$REX['TABLE_PREFIX'].'firedepartment_config_unit` ORDER BY `name` ASC';
 				$select->addSqlOptions($query);
-			//End - field for unit_id
+			//End - field for unit_ids
 			
 			//Start - field for vehicle_id
 				$field = &$form->addSelectField('config_vehicle_ids');
-				$field->setLabel('Vehicle ID'); //todo: translate
+				$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_config_vehicle_ids_label'));
 				$field->setAttribute('multiple', 'multiple');
 				$select = &$field->getSelect();
 				$select->setSize(5);
@@ -65,19 +64,22 @@
 			//End - field for vehicle_id
 			
 			$field = &$form->addTextareaField('report_short', null, ['rows' => 2]);
-			$field->setLabel('Kurzbericht');//todo: translate
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_report_short_label'));
 			
 			$field = &$form->addTextareaField('report_long', null, ['rows' => 6]);
-			$field->setLabel('Bericht');//todo: translate
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_report_long_label'));
 			
 			$field = &$form->addTextField('place');
-			$field->setLabel('Einsatzort');//todo: translate
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_place_label'));
 			
 			$field = &$form->addTextField('start_date', null, ['class' => 'date']);
-			$field->setLabel('Alarmierung');//todo: translate
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_start_date_label'));
 			
 			$field = &$form->addTextField('end_date', null, ['class' => 'date']);
-			$field->setLabel('Einsatzende');//todo: translate
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_end_date_label'));
+			
+			$field = &$form->addMedialistField('images');
+			$field->setLabel($addon_i18n->msg('rex_firedepartment_ctype_operations_formfield_images_label'));
 			
 			if($func == 'edit') {
 				$form->addParam('id', $id);
